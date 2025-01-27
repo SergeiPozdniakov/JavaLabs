3908ac5f3ccaa0b285c66559b19f495613dbd14f
SergeiPozdniakov <143334225+SergeiPozdniakov@users.noreply.github.com>
2025-01-28 00:02:12 +0300

    Update AverageScoreCache.java

--- students/src/main/java/org/example/students/AverageScoreCache.java
+++ students/src/main/java/org/example/students/AverageScoreCache.java
@@ -1,13 +1,12 @@
 package org.example.students;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AverageScoreCache {
-    private final Map<String, Double> cache = new HashMap<>();
+    private final Map<String, Double> cache = new LRUCache<>(2);
 
     public Double get(String subject) {
         return cache.get(subject);
@@ -23,5 +22,20 @@ public class AverageScoreCache {
 
     public boolean contains(String subject) {
         return cache.containsKey(subject);
+    }
+
+class LRUCache<KEY, VALUE> extends LinkedHashMap<KEY, VALUE> {
+
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<KEY, VALUE> eldest) {
+        return size() > capacity;
+    }
     }
 }
