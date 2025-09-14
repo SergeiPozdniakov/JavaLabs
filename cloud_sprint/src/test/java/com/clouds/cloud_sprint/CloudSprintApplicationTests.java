d7dc23d2f0a347cc4ae38bf9b7c399eb6b19f6fa
SergeiPozdniakov <143334225+SergeiPozdniakov@users.noreply.github.com>
2025-09-14 13:17:13 +0300

    Jenckins test

--- cloud_sprint/src/test/java/com/clouds/cloud_sprint/CloudSprintApplicationTests.java
+++ cloud_sprint/src/test/java/com/clouds/cloud_sprint/CloudSprintApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class CloudSprintApplicationTests {
 
 	@Test
-    public void contextLoads() {
+    public void contextLoadsApp() {
 	}
 
 }
